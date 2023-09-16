76c61bd4d6ba311c3f878e4e9a6716855ea1a50b
Pablo Ortiz <63486413+PabloOrtiz07@users.noreply.github.com>
2023-09-16 11:01:50 -0300

    add new features

--- IntegratorSofttek/Logic/ProjectProfile.cs
+++ IntegratorSofttek/Logic/ProjectProfile.cs
@@ -8,9 +8,29 @@ public class ProjectProfile : Profile
     public ProjectProfile()
     {
         CreateMap<ProjectDTO, Project>()
+            .ForMember(dest => dest.Status, opt => opt.MapFrom(src => MapStatusStringToEnum(src.Status)))
             .ForMember(dest => dest.IsDeleted, opt => opt.MapFrom(src => false))
             .ForMember(dest => dest.DeletedTimeUtc, opt => opt.MapFrom(src => (DateTime?)null));
 
-        CreateMap<Project, ProjectDTO>();
+        CreateMap<Project, ProjectDTO>()
+            .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status.ToString()));
+
+
     }
+
+    private ProjectStatus MapStatusStringToEnum(string status)
+    {
+        switch (status.ToLower())
+        {
+            case "pending":
+                return ProjectStatus.Pending;
+            case "confirmed":
+                return ProjectStatus.Confirmed;
+            case "finished":
+                return ProjectStatus.Finished;
+            default:
+                throw new ArgumentException($"Invalid status string: {status}");
+        }
+    }
+
 }
