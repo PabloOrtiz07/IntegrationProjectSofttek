baf12671323d665edcf9e15872103563c343143a
Pablo Ortiz <63486413+PabloOrtiz07@users.noreply.github.com>
2023-09-20 14:21:08 -0300

    Mapped RoleDTOs Information to UserDTOs

---
+++ IntegratorSofttek/Logic/RoleProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using IntegratorSofttek.DTOs;
+using IntegratorSofttek.Entities;
+using IntegratorSofttek.Helper;
+
+public class RoleProfile : Profile
+{
+    public RoleProfile()
+    {
+        CreateMap<RoleDTO, Role>();
+
+        CreateMap<Role, RoleDTO>();
+
+    }
+}
